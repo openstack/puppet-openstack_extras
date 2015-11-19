cdeede97b90b5bbb0a54125ec68c36cf7e249370
Emilien Macchi <emilien@redhat.com>
2015-11-19 11:38:39 +0100

    Acceptance: use common bits from puppet-openstack-integration
    Change-Id: I5e81294345261d572b245ff6015a9f7c7de9582a

--- spec/acceptance/openstack_extras_repo_spec.rb
+++ spec/acceptance/openstack_extras_repo_spec.rb
@@ -6,15 +6,9 @@ describe 'openstack_extras::repo::*' do
 
     release = 'liberty'
     it 'should work with no errors' do
-      if fact('operatingsystem') == 'CentOS'
-        operatingsystem = 'redhat'
-      else
-        operatingsystem = fact('operatingsystem').downcase
-      end
       pp= <<-EOS
-        class { '::openstack_extras::repo::#{fact('osfamily').downcase}::#{operatingsystem}':
-          release => #{release}
-        }
+        include ::openstack_integration
+        include ::openstack_integration::repos
       EOS
 
 
